18dff754ddfe7633b7aff2fa151d4ebae87c674b
Аслан Джумаев <dragmagaslanjum007@gmail.com>
2023-11-27 19:27:43 +0700

    Добавил проверки, но надо доделать

--- proj_1OPO/FigureList.cpp
+++ proj_1OPO/FigureList.cpp
@@ -20,6 +20,39 @@ void FigureList::push_back(Figure* figure) {
     }
 }
 
+void FigureList::removeIfMatches(const std::string& condition) {
+    if (head == nullptr)
+        return;
+
+    FigureNode* curr = head;
+    FigureNode* prev = nullptr;
+
+    while (!curr->figure->matchesCondition(condition)) {
+        if (curr->next == head) {
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (curr->next == head) {
+        delete curr;
+        head = nullptr;
+    }
+    else if (curr == head) {
+        prev = head;
+        while (prev->next != head)
+            prev = prev->next;
+        head = curr->next;
+        prev->next = head;
+        delete curr;
+    }
+    else {
+        prev->next = curr->next;
+        delete curr;
+    }
+}
+
 void FigureList::printAll() const {
     if (!head) {
         return;
--- proj_1OPO/proj_1OPO.cpp
+++ proj_1OPO/proj_1OPO.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#define _CRTDBG_MAP_ALLOC
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -8,9 +9,43 @@
 #include "Cylinder.h"
 #include "FigureNode.h"
 #include "Sphere.h"
-
+#include <stdlib.h>
+#include <crtdbg.h>
 
 using namespace std;
+
+bool isString(const std::string& input) {
+    int y = 0, size;
+    size = input.length();
+    for (char c : input) {
+        if (!isdigit(c)) {
+            y++;
+        }
+    }
+    return size == y;
+}
+
+bool isNumber(int& x, istringstream& iss)
+{
+    iss >> x;
+    if (iss.fail() && !isspace(iss.peek())) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+bool isNumber_D(double& x, istringstream& iss)
+{
+    iss >> x;
+    if (iss.fail() && !isspace(iss.peek())) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
 
 int main() {
     FigureList figureList;
@@ -22,6 +57,7 @@ int main() {
 
     string line;
     while (getline(inputFile, line)) {
+
         istringstream iss(line);
         string command;
         iss >> command;
@@ -29,28 +65,26 @@ int main() {
         if (command == "ADD") {
             double density;
             string owner;
-            iss >> density >> owner;
-
+            if (isNumber_D(density, iss) && iss >> owner && isString(owner));
+            else continue;
             string type;
             iss >> type;
             if (type == "Sphere") {
                 int radius;
-                iss >> radius;
-                figureList.push_back(new Sphere(density, owner, radius));
+                if (isNumber(radius, iss)) figureList.push_back(new Sphere(density, owner, radius));
+                else continue;
             }
             else if (type == "Cuboid") {
                 int l1, l2, l3;
-                iss >> l1 >> l2 >> l3;
-                figureList.push_back(new Cuboid(density, owner, l1, l2, l3));
+                if (isNumber(l1, iss) && isNumber(l2, iss) && isNumber(l3, iss)) figureList.push_back(new Cuboid(density, owner, l1, l2, l3));
+                else continue;
             }
             else if (type == "Cylinder") {
                 int bx, by, h, br;
-                iss >> bx >> by >> h >> br;
-                figureList.push_back(new Cylinder(density, owner, bx, by, h, br));
+                if (isNumber(bx, iss) && isNumber(by, iss) && isNumber(h, iss) && isNumber(br, iss)) figureList.push_back(new Cylinder(density, owner, bx, by, h, br));
+                else continue;
             }
-            else {
-                cerr << "ADD Error: " << line << endl;
-            }
+            else continue;
         }
         else if (command == "REM") {
             string condition;
@@ -65,9 +99,7 @@ int main() {
             cerr << "Command Error: " << command << endl;
         }
     }
-
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+    _CrtDumpMemoryLeaks();
     return 0;
 }
-//разбить по файлам, отдельано список, фигуры, отдельно main, remove if match, функция удаления спереди и сзади как стандартную back и front
-//valgrint динамический оптимизатор кода, чтобы перестал течь память.
-//отедльное коммит в отедльную ветку.
